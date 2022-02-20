9a12f70dbb1c93b57c08d50809400aea287b3eb1
John Nguyen <mjrnguyen@gmail.com>
2022-02-20 11:48:52 -0800

    london

--- src/london_baseline.py
+++ src/london_baseline.py
@@ -2,3 +2,22 @@
 #   example in the dev set.
 # Hint: Make use of existing code.
 # Your solution here should only be a few lines.
+from tqdm import tqdm
+import utils
+import argparse
+
+argp = argparse.ArgumentParser()
+
+argp.add_argument('--eval_corpus_path',
+    help="Path of the corpus to evaluate on", default=None)
+
+args = argp.parse_args()
+
+preds = []
+
+for x in tqdm(open(args.eval_corpus_path)):
+	preds.append('London')
+
+total, correct = utils.evaluate_places(args.eval_corpus_path, preds)
+
+print('Correct: {} out of {}: {}%'.format(correct, total, correct/total*100))
